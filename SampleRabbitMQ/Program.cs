b75f841bd8ba56e3ccdf13dd4a9bf5f76e919583
Edvinas K <edvinas.kesminas@gmail.com>
2018-10-24 22:07:01 +0300

    [RMQ-0001] Initial RabbitMQ console app creation

--- SampleRabbitMQ/Program.cs
+++ SampleRabbitMQ/Program.cs
@@ -4,9 +4,22 @@ namespace SampleRabbitMQ
 {
     class Program
     {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Password = "guest";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory() {
+                Password = Password,
+                UserName = UserName,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+
         }
     }
 }
